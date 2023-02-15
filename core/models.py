2452f1353ed8bfcc3ccf253f155d6f281761eee2
dany <my@gmail.ru>
2023-02-15 09:34:59 +0300

    Добавил тип заданий с ответом текстовое поле

--- core/models.py
+++ core/models.py
@@ -70,7 +70,7 @@ class Task(models.Model):
     )
     chapter = models.ForeignKey(to=Chapter, on_delete=models.CASCADE)
     def __str__(self):
-        return f"{self.title}"
+        return f"{self.description}"
 class Question(models.Model):
     task = models.ForeignKey(to=Task, on_delete=models.CASCADE)
     description = models.TextField()
